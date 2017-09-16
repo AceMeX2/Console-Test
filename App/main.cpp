fe8c78d80799f41068bc6850ea2bc055071d1b41
AceMeX2 <ryanunternahrer@gmail.com>
2017-09-17 09:15:48 +1200

    Basic Functionality

--- App/main.cpp
+++ App/main.cpp
@@ -7,11 +7,36 @@ using namespace std;
 
 int main(){
     
-    cout << "Hello World" << endl;
+    cout << "Hello" << endl;
     
-    float PI = 3.14159;
+    cout << "What's Your Name?" << endl;
     
-    cout << "PI " << PI << endl;
+    string name;
+    
+    cin >> name;
+    
+    cout << "Hi " << name << " my name is CPP-Bot" << endl;
+    
+    cout << "How old are you?" << endl;
+    
+    int age;
+    
+    cin >> age;
+    
+    cout << "Your " << age << " well I'm not even one as I was created in 2017." << endl;
+    
+    if (age > 17)
+    {
+        cout << "Adult Mode Activated. Hello Big Boy and or Girl." << endl;
+        
+        cout << "Whoops you weren't meant to see that." << endl;
+    }
+    
+    else {
+        cout << "Child Mode Activated." << endl;
+        
+        cout << "Whoops another glitch." << endl;
+    }
     
     return 0;
     
